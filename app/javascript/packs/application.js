423ef2b6e5b22aee198ed4899842884a2d1992a7
Georg Ledermann <mail@georg-ledermann.de>
2019-12-05 06:14:53 +0100

    Add favicon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,8 +12,8 @@ import '../styles/application.scss'
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
 
 import Vue from 'vue'
 import VueMeta from 'vue-meta'

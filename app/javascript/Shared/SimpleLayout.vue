706c8823c37a4675fd493882defdd082def93df4
Georg Ledermann <mail@georg-ledermann.de>
2019-12-13 18:00:03 +0100

    Use toast notifications for flash messages

---
+++ app/javascript/Shared/SimpleLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <portal-target
+      name="modal"
+      slim
+    />
+    <portal-target
+      name="dropdown"
+      slim
+    />
+
+    <notifications
+      group="default"
+      position="top center"
+      width="75%"
+      classes="my-notification-style"
+    />
+
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,6 +29,38 @@ axios.defaults.xsrfHeaderName = 'X-CSRF-Token'
 import MatomoTracker from '@/utils/matomo-tracker'
 const matomo = new MatomoTracker()
 
+import Notifications from 'vue-notification'
+Vue.use(Notifications)
+
+function handleNotications(props) {
+  setTimeout(() => {
+    if (props.flash.success) {
+      Vue.notify({
+        group: 'default',
+        text: props.flash.success,
+        type: 'success',
+      })
+    } else if (props.flash.alert) {
+      Vue.notify({
+        group: 'default',
+        text: props.flash.alert,
+        type: 'error',
+      })
+    } else if (Object.keys(props.errors).length === 1) {
+      Vue.notify({
+        group: 'default',
+        text: 'There is one form error.',
+        type: 'error',
+      })
+    } else if (Object.keys(props.errors).length > 1) {
+      Vue.notify({
+        group: 'default',
+        text: `There are ${Object.keys(props.errors).length} form errors.`,
+        type: 'error',
+      })
+    }
+  }, 100)
+}
 
 import { InertiaApp } from '@inertiajs/inertia-vue'
 Vue.use(InertiaApp)
@@ -48,6 +80,8 @@ new Vue({
       initialPage: JSON.parse(app.dataset.page),
       resolveComponent: name => import(`@/Pages/${name}`).then(module => module.default),
       transformProps: props => {
+        handleNotications(props)
+
         if (matomo.enabled)
           // Wait a bit to allow VueMeta to update the document.title
           setTimeout(() => {

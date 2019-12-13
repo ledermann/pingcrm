734982c15c6591873d9cd0603d9956cdb8bd9b4c
Georg Ledermann <mail@georg-ledermann.de>
2019-12-13 06:04:31 +0100

    Remove unneeded Rails JS packages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,6 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require('@rails/ujs').start()
-require('@rails/activestorage').start()
-
 import '../styles/application.scss'
 
 // Uncomment to copy all static images under ../images to the output folder and reference

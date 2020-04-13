793700284213e812a96f714746536abef6c0d5be
Georg Ledermann <mail@georg-ledermann.de>
2020-04-13 08:03:48 +0200

    Enable Content Security Policy (CSP)

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,19 +4,31 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-# Rails.application.config.content_security_policy do |policy|
-#   policy.default_src :self, :https
-#   policy.font_src    :self, :https, :data
-#   policy.img_src     :self, :https, :data
-#   policy.object_src  :none
-#   policy.script_src  :self, :https
-#   policy.style_src   :self, :https
-#   # If you are using webpack-dev-server then specify webpack-dev-server host
-#   policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
+Rails.application.config.content_security_policy do |policy|
+  policy.default_src :none
+  policy.font_src     :self
+  policy.img_src(*[   :self, :data, ENV['MATOMO_HOST']].compact)
+  policy.object_src   :none
+  policy.style_src    :self, :unsafe_inline
+  policy.form_action  :self
+  policy.manifest_src :self
 
-#   # Specify URI for violation reports
-#   # policy.report_uri "/csp-violation-report-endpoint"
-# end
+  if Rails.env.development?
+    # If you are using webpack-dev-server then specify webpack-dev-server host
+    policy.connect_src :self, 'http://localhost:3035', 'ws://localhost:3035'
+
+    # Inertia.js uses inline scripts to display error modal in development
+    policy.script_src :self, :unsafe_inline, 'https://polyfill.io'
+  else
+    policy.connect_src :self
+    policy.script_src(*[:self, 'https://polyfill.io', ENV['MATOMO_HOST']].compact)
+  end
+
+  policy.base_uri :self
+
+  # Specify URI for violation reports
+  # policy.report_uri "/csp-violation-report-endpoint"
+end
 
 # If you are using UJS then enable automatic nonce generation
 # Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }

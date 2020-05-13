a637879c058cb6e99d35517a22c514cc5a80390c
Georg Ledermann <mail@georg-ledermann.de>
2020-05-13 07:45:19 +0200

    RuboCop: Enable new cops for 0.83

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module Pingcrm
       Class.new(ActionController::Base) do # rubocop:disable Rails/ApplicationController
         def show
           render inertia: 'Error', props: {
-            status: request.path_info[1..-1].to_i
+            status: request.path_info[1..].to_i
           }
         end
       end.action(:show).call(env)

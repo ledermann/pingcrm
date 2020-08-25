d3374312e187110af577f608d02ce3aefbbf6b51
Georg Ledermann <mail@georg-ledermann.de>
2020-08-25 06:45:09 +0200

    Remove unused code

--- app/javascript/Layouts/Main.vue
+++ app/javascript/Layouts/Main.vue
@@ -3,7 +3,6 @@
     <div class="flex flex-col">
       <div
         class="h-screen flex flex-col"
-        @click="hideDropdownMenus"
       >
         <div class="md:flex flex-shrink-0">
           <div class="bg-indigo-900 md:flex-shrink-0 md:w-56 px-6 py-4 flex items-center justify-between md:justify-center">
@@ -122,16 +121,12 @@ export default {
   },
   data() {
     return {
-      showUserMenu: false,
       accounts: null,
     }
   },
   methods: {
     url() {
       return location.pathname.substr(1)
-    },
-    hideDropdownMenus() {
-      this.showUserMenu = false
     },
   },
 }

3de5a888041bc4e29af5cd6985e93e88072a3d6f
Georg Ledermann <georg@ledermann.dev>
2021-11-05 09:47:32 +0100

    CSP/style_src: Use hash instead of unsafe_inline

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -5,7 +5,6 @@
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
 Rails.application.config.content_security_policy do |policy|
-  policy.default_src :none
   policy.font_src     :self
   policy.img_src(*[   :self, :data, ENV['MATOMO_HOST']].compact)
   policy.object_src   :none
@@ -19,12 +18,13 @@ Rails.application.config.content_security_policy do |policy|
     # Inertia.js uses inline scripts to display error modal in development
     policy.script_src :self, :unsafe_inline
   else
+    policy.default_src :none
     policy.connect_src(*[:self, ENV['MATOMO_HOST']].compact)
     policy.script_src(*[:self, ENV['MATOMO_HOST']].compact)
+    policy.style_src :self,
+                     # @inertiajs/progress uses inline styles to display progress bar
+                     "'sha256-kCeyw5rRT2DINADvWYmAhXLhQs4dKZrnn2sofIDmprs='"
   end
-
-  # @inertiajs/progress uses inline styles
-  policy.style_src :self, :unsafe_inline
 
   policy.base_uri :self
 

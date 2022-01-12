fa312a1eb7b3c48de87ef4a087b129ad1381ea9a
Georg Ledermann <georg@ledermann.dev>
2022-01-12 07:08:09 +0100

    Fix flaky tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,5 +3,7 @@ require "test_helper"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include Devise::Test::IntegrationHelpers
 
-  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400] do |driver_option|
+    driver_option.add_argument('--disable-ipc-flooding-protection')
+  end
 end

945adb10ad0d116b6a52583ad9b5be4dea699011
Georg Ledermann <georg@ledermann.dev>
2022-07-16 10:30:37 +0200

    Use localhost:3000 for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,4 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
-
-  config.hosts << 'pingcrm.test'
 end

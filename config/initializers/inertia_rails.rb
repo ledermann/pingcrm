0362edc8afef8fa18be6236e728baec4cbc3acaa
Georg Ledermann <georg@ledermann.dev>
2022-07-16 15:07:26 +0200

    Use ViteRuby for digesting

--- config/initializers/inertia_rails.rb
+++ config/initializers/inertia_rails.rb
@@ -1,19 +1,3 @@
-require 'digest'
-
 InertiaRails.configure do |config|
-  def checksum(dir)
-    files = Dir["#{dir}/**/*"].reject { |f| File.directory?(f) }
-    content = files.map { |f| File.read(f) }.join
-    Digest::MD5.hexdigest(content)
-  end
-
-  asset_path = if Rails.env.test?
-    Rails.public_path.join('vite-test')
-  elsif Rails.env.production?
-    Rails.public_path.join('vite')
-  elsif Rails.env.development?
-    Rails.root.join('app', 'vite-dev')
-  end
-
-  config.version = checksum(asset_path)
+  config.version = ViteRuby.digest
 end

de744d774a229b193e2be043537ca54c1b79aebb
Georg Ledermann <georg@ledermann.dev>
2022-07-16 15:33:51 +0200

    Remove application.css from entrypoint

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -3,6 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import '~/styles/application.css';
+
 import { createApp, h } from 'vue';
 
 import { Inertia } from '@inertiajs/inertia';

b02ba002f65ea038d3f2f76584dd4696e2b50a29
Georg Ledermann <georg@ledermann.dev>
2024-08-08 06:59:21 +0200

    :arrow_up: ESLint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import pluginVue from 'eslint-plugin-vue';
+
+export default [
+  js.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      'vue/no-unused-vars': 'error',
+      'vue/script-setup-uses-vars': 'error',
+      'vue/multi-word-component-names': 'off',
+      'vue/require-default-prop': 'off',
+      'vue/no-reserved-component-names': 'off',
+      'vue/max-attributes-per-line': 'off',
+      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    },
+  },
+  {
+    ignores: [
+      '.ruby-lsp/',
+      '.yarn/',
+      'app/javascript/routes.js',
+      'config/',
+      'db/',
+      'log/',
+      'node_modules/',
+      'public/',
+      'tmp/',
+      'vendor/',
+    ],
+  },
+];
